8bbe7e5a4f8ee85d4b1a9a4ab211e606728a85d1
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-08-06 19:37:56 -0300

    Fixing folders misdirections

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AuthUserProvider } from './hooks/useAuth';
+import { AuthUserProvider } from './src/hooks/useAuth';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
