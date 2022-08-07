254a25e3cbcb2984be77cb2cb17ce2f2ecfaf5ea
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-08-07 13:41:51 -0300

    Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,14 @@
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import Colors from "../constants/Colors";
 import Size from "../constants/Layout";
 
 interface Props {
     id?: string;
-    shippedDate?: string;
-    arrivalDate?: string;
+    shippedDate: string;
+    arrivalDate: string;
     itemName: string;
     value?: number;
 }
@@ -16,18 +20,62 @@ const Card: React.FC<Props> = ({
     itemName,
     value
 }) => {
+
     return (
         <View style={styles.container}>
-            <Text>{itemName}</Text>
+            <Text style={styles.title}>{itemName}</Text>
+            <View style={styles.datesView}>
+                <Text style={styles.shipped}>{`Shipped: ${shippedDate}`}</Text>
+                <FontAwesomeIcon
+                    icon={faArrowRightLong}
+                    size={18}
+                    style={styles.arrow}
+                />
+                <Text style={styles.arrival}>{`Arrival: ${arrivalDate}`}</Text>
+            </View>
+            <View style={styles.price}>
+                <Text>{value ? `Price: $${value.toFixed(2)}` : "Price: --.--"}</Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        height: Size.window.height / 5,
-        width: "100%",
-        backgroundColor: "#fff",
+        height: Size.window.height / 6,
+        marginHorizontal: 15,
+        backgroundColor: "#eee",
+        elevation: 10,
+        borderRadius: 8,
+        padding: 15,
+        justifyContent: "space-between",
+        marginTop: 10
+    },
+    title: {
+        textAlign: "center",
+        color: "#000",
+        fontSize: 17,
+        fontWeight: "600"
+    },
+    datesView: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        marginVertical: 10,
+    },
+    shipped: {
+        color: "#FF6E1C",
+        fontSize: 15,
+    },
+    arrow: {
+        alignSelf: "center"
+    },
+    arrival: {
+        color: "green",
+        fontSize: 15,
+        fontWeight: "500"
+    },
+    price: {
+        alignSelf: "flex-end"
     }
 })
 
